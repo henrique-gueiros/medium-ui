db0ae5032bfe672bd4dda44335613a20e880958d
henrique-gueiros <riquewg@gmail.com>
2025-11-11 12:14:41 -0300

    feat: Enhance post display with author information and like functionality

--- js/controllers/home-controller.js
+++ js/controllers/home-controller.js
@@ -1,4 +1,4 @@
-app.controller('homeController', function ($scope, postService, $modal, $rootScope) {
+app.controller('homeController', function ($scope, postService, userService, $modal, $rootScope) {
   $scope.isUserLoggedIn = $rootScope.userLogged;
   $scope.posts = [];
 
@@ -7,6 +7,19 @@ app.controller('homeController', function ($scope, postService, $modal, $rootSco
       .then(function (response) {
         console.log("✅ Sucesso!", response.data);
         $scope.posts = response.data.data.posts;
+
+        // Buscar autores para cada post
+        $scope.posts.forEach(function (post) {
+          userService.getUserName(post.user_id)
+            .then(function (response) {
+              post.authorName = response.data.data;
+            })
+            .catch(function (error) {
+              console.error("Erro ao buscar usuário:", error);
+            })
+        });
+
+
       })
       .catch(function (error) {
         console.error("Erro na requisição:", error);
